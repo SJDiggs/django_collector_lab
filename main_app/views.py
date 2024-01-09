455c58f1646c75322c3e9185e2dca79b1bc3c420
stevediggs70 <stevediggs670@gmail.com>
2024-01-09 16:18:32 -0500

    completed part1, minor syntax updates

--- main_app/views.py
+++ main_app/views.py
@@ -2,8 +2,8 @@ from django.shortcuts import render
 
 # Create your views here.
 motorcycles = [
-  {'name': 'Kawasaki', 'breed': 'Z650RS', 'description': 'fun middle-weight commuter', 'age': 2023},
-  {'name': 'Honda', 'breed': 'CR125R', 'description': 'screaming motox champ', 'age': 1985},
+  {'name': 'Kawasaki', 'model': 'Z650RS', 'description': 'fun middle-weight commuter', 'year': 2023},
+  {'name': 'Honda', 'model': 'CR125R', 'description': 'screaming motox champ', 'year': 1985},
 ]
 
 def home(request) :
@@ -12,7 +12,7 @@ def home(request) :
 def about(request):
     return render(request, 'about.html')
 
-def motorcycle_index(request):
-    return render(request, 'motorcycle/index.html', {
+def motorcycles_index(request):
+    return render(request, 'motorcycles/index.html', {
         'motorcycles': motorcycles
     })
