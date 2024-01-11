b16929a0a894471fc0f4263b97a55e171a47bcc4
stevediggs70 <stevediggs670@gmail.com>
2024-01-10 20:17:40 -0500

    connected to db, created detail card

--- main_app/views.py
+++ main_app/views.py
@@ -1,18 +1,27 @@
 from django.shortcuts import render
 
 # Create your views here.
-motorcycles = [
-  {'name': 'Kawasaki', 'model': 'Z650RS', 'description': 'fun middle-weight commuter', 'year': 2023},
-  {'name': 'Honda', 'model': 'CR125R', 'description': 'screaming motox champ', 'year': 1985},
-]
+from .models import Motorcycle
 
+# motorcycles = [
+#   {'name': 'Kawasaki', 'model': 'Z650RS', 'description': 'fun middle-weight commuter', 'year': 2023},
+#   {'name': 'Honda', 'model': 'CR125R', 'description': 'screaming motox champ', 'year': 1985},
+# ]
+# HOME VIEW
 def home(request) :
     return render(request, 'home.html')
 
+# ABOUT VIEW
 def about(request):
     return render(request, 'about.html')
 
+# MOTORCYCLE INDEX
 def motorcycles_index(request):
     return render(request, 'motorcycles/index.html', {
         'motorcycles': motorcycles
     })
+
+# MOTORCYCLE DETAIL
+def motorcycles_detail(request, motorcycle_id):
+  motorcycle = Motorcycle.objects.get(id=motorcycle_id)
+  return render(request, 'motorcycles/detail.html', { 'motorcycle': motorcycle })
